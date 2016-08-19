4f7069098df5bd019eedb13ab09b6ef2961e6676
cledic <clemente.dicaprio@gmail.com>
2016-08-19 19:51:02 +0200

    Create power_down_cpu.sh

---
+++ power_down_cpu.sh
@@ -0,0 +1,4 @@
+echo 0 >/sys/devices/system/cpu/cpu3/online
+echo 0 >/sys/devices/system/cpu/cpu2/online
+echo 0 >/sys/devices/system/cpu/cpu1/online
+echo 132000 >/sys/devices/platform/sunxi-ddrfreq/devfreq/sunxi-ddrfreq/userspace/set_freq
