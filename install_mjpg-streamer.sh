7b7c4c1a1eb009f633ed54a1ac8ae158741bf289
cledic <clemente.dicaprio@gmail.com>
2016-08-19 23:08:56 +0200

    Create install_mjpg-streamer.sh

---
+++ install_mjpg-streamer.sh
@@ -0,0 +1,7 @@
+wget http://lilnetwork.com/download/raspberrypi/mjpg-streamer.tar.gz
+tar xvzf mjpg-streamer.tar.gz
+sudo apt-get install libjpeg8-dev
+sudo apt-get install imagemagick
+cd mjpg-streamer/mjpg-streamer
+make
+./mjpg_streamer -i "./input_uvc.so" -o "./output_http.so -w ./www"
