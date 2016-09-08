6aca03a68011be5319f73eb3c5ebdc52f1f0d315
cledic <clemente.dicaprio@gmail.com>
2016-09-08 09:03:00 +0200

    Update cpu_off.sh

--- cpu_off.sh
+++ cpu_off.sh
@@ -1,4 +1,6 @@
-echo 0 >/sys/devices/system/cpu/cpu3/online
-echo 0 >/sys/devices/system/cpu/cpu2/online
-echo 0 >/sys/devices/system/cpu/cpu1/online
+#!/bin/sh
+
+#
+for i in 3 2 1; do echo 0 >/sys/devices/system/cpu/cpu${i}/online; done
 echo 132000 >/sys/devices/platform/sunxi-ddrfreq/devfreq/sunxi-ddrfreq/userspace/set_freq
+
